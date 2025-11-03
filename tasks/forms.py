e8f934fb53e8117cda0895fdb29c319c9f6b766a
Ntarekp <kpntare@gmail.com>
2025-11-03 23:59:51 +0200

    Added all possible files and ready for testing

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,8 +4,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title','description','due_date']
+        fields = ['title', 'description', 'due_date']
         widgets = {
-            'due_date': forms.DateInput(attrs={'type','date'}),
-            'description': forms.Textarea(attrs={'rows':4}),
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
         }
